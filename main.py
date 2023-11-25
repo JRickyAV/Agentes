b6b08fa241161ce36d76bde48f454e5acc008801
JRickyAV <ricky_a_2002@hotmail.com>
2023-11-25 17:53:17 -0600

    Ultima version

--- main.py
+++ main.py
@@ -18,32 +18,26 @@ class Sensor:
 
         self.hilo_recibir = threading.Thread(target=self._recibirMensajes, daemon=True)
         self.hilo_recibir.start()
-
-
-
     
     def _recibirMensajes(self):
         while True:
+      
             data, addr = self.client_socket.recvfrom(1024)
             self.mensaje = data.decode()
             # Aquí puedes realizar cualquier lógica para procesar el mensaje recibido
             # y cambiar la variable de la clase según tus necesidades.
             print(f'Mensaje recibido de {addr}: {self.mensaje}')
 
-
-
-
     def send_udp_message(self,mensaje):
         # Enviar un mensaje al servidor
-        self.client_socket.sendto(mensaje.encode(), (self.host2, self.port2))
-
-        
+        self.client_socket.sendto(mensaje.encode(), (self.host2, self.port2))        
 
 def main():
     sensor1 = Sensor(1,8081,"192.168.1.67",8080)
-    sensor1.send_udp_message("Hola")
+    sensor1.send_udp_message("Temperatura:23°")
+
     while True:
         time.sleep(1)
     
 
-
+main()
